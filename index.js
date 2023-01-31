f70a8e49255d11fc353664454ee9e8ef834721db
lucasgonzalezgagliardo <lucas.e.gonzalez@gmail.com>
2023-01-31 14:09:16 +0100

    el pikachu salta

--- index.js
+++ index.js
@@ -5,19 +5,23 @@ window.onload = () => {
             this.x = 100;
             this.y = 360;
             this.w = 75;
-            this.h = 75;
+            this.h = 50;
             this.vel = 0;
-            this.gravity = 0;
+            this.gravity = 2;
+            this.jumping = false;
 
             this.imgPlayer = document.createElement("img");
-            this.imgPlayer.src = "images/dino.png"
+            this.imgPlayer.src = "images/pika.gif"
 
         }
         print(ctx) {
 
             ctx.drawImage(this.imgPlayer, this.x, this.y, this.w, this.h);
         }
-        jump() { }
+        jump() { 
+            this.vel = -25;
+            this.jumping = true;
+        }
     }
 
     class Fondo {
@@ -27,7 +31,7 @@ window.onload = () => {
             this.w = canvas.width;
             this.h = canvas.height;
             this.imgBack = document.createElement("img");
-            this.imgBack.src = "images/background.jpg"
+            this.imgBack.src = "images/fondo.jpeg"
             this.vel = 2
         }
         print(ctx) {
@@ -110,7 +114,7 @@ window.onload = () => {
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         }
         print() {
-            console.log(this.ctx);
+            // console.log(this.ctx);
             //fondo
             this.fondo.print(this.ctx);
             //coche
@@ -128,6 +132,14 @@ window.onload = () => {
 
             //     //cambio posiciones
             this.obstaculo.move();
+
+            this.player.y += this.player.vel;
+            this.player.vel += this.player.gravity;
+            if (this.player.y >= 360) {
+              this.player.y = 360;
+              this.player.vel = 0;
+              this.player.jumping = false;
+            }  
             // // controlo colisiones
             // if (!(this.player.x + this.player.w < obstaculo.x ||
             //     this.player.x > player.x + player.w ||
@@ -139,18 +151,27 @@ window.onload = () => {
 
     }
 
-
-
+    
     let juego = new Juego();
-
+    
     document.getElementById('start-button').onclick = () => {
         startGame();
     };
-
+    
     function startGame() {
         juego.start();
     }
+    
+    //BARRA ESPACIADORA!!!
+    
+    document.addEventListener("keydown", function(event) {
+        if (event.code === "Space" && !juego.player.jumping) {
 
-
+            juego.player.jump()
+        }
+      });
+    
+    //BARRA ESPACIADORA!!!
+    
 }
 
